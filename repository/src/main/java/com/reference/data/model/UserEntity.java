cdbb565e05a58fb89e174a8fab75b4e64d494464
Alex Dimitrijeski <alex.dimitrijeski@gmail.com>
2024-03-22 09:39:52 -0400

    adding to gitignore, removing unused import.

--- repository/src/main/java/com/reference/data/model/UserEntity.java
+++ repository/src/main/java/com/reference/data/model/UserEntity.java
@@ -3,7 +3,6 @@ package com.reference.data.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
